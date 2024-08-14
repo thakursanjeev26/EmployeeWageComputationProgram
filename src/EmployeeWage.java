4cb996bad8da968fbe1f5199a5f10cc3082e3c6d
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-14 16:30:17 +0530

    Added: Employee Presence Check Function

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,11 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To EmployeeWageComputationProgram");
+        int IS_FULL_TIME=1;
+        double empCheck=Math.floor(Math.random()*10)%2;
+        if(empCheck==IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
