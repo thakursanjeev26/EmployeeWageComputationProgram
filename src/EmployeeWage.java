6d33ef16500c3c4a929f0c8c4b4ceda9b12c61de
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-14 16:54:25 +0530

    Added:Part Time Employee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,17 +1,23 @@
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome To EmployeeWageComputationProgram");
-        int IS_FULL_TIME=1;
+        int IS_PART_TIME=1;
+        int IS_FULL_TIME=2;
         int EMP_RATE_PER_HOUR=20;
         int empHrs;
         int empWage;
         double empCheck=Math.floor(Math.random()*10)%2;
-        if(empCheck==IS_FULL_TIME){
-            System.out.println("Employee is Present");
+        if(empCheck==IS_PART_TIME){
+            System.out.println("Employee IS Present");
+            empHrs=4;
+        }
+        else if (empCheck==IS_FULL_TIME) {
+            System.out.println("Employee IS Present");
             empHrs=8;
         }
+
         else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee IS Absent");
             empHrs = 0;
         }
         empWage=empHrs*EMP_RATE_PER_HOUR;
