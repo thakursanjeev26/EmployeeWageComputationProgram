cf40f09ba1d626b0bb9b3262666f64f1ee504d81
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-08-21 11:07:36 +0530

    Added: Condition To Calculate EmployeeWage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,14 +3,19 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME=2;
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int NUM_OF_WORKING_DAYS=2;
+    public static final int MAX_HRS_IN_MONTH=10;
 
     public static void main(String[] args) {
+
         System.out.println("Welcome To EmployeeWageComputationProgram");
 
         int empHrs;
-        int empWage;
-        int totalEmpWage=0;
-        for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
+        int totalEmpHrs=0;
+        int totalWorkingDays=0;
+
+
+       while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+           totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_PART_TIME:
@@ -22,10 +27,11 @@ public class EmployeeWage {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage+=empWage;
-            System.out.println("Emp Wage:" + empWage);
+            totalEmpHrs+=empHrs;
+            System.out.println("Days#: "+ totalWorkingDays + " Emp Hr:" +empHrs);
+
         }
+        int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
         System.out.println("Total EMP Wage:"+totalEmpWage);
     }
 }
